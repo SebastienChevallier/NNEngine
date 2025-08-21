e09947d77bad3783d41ee3f657af063dfab8fa33
Sebastien chevallier <44536776+SebastienChevallier@users.noreply.github.com>
2025-08-21 13:31:05 +0200

    Update Main.cpp

--- examples/src/Main.cpp
+++ examples/src/Main.cpp
@@ -5,10 +5,10 @@ int main() {
     NNE::Systems::Application app;
 
         NNE::AEntity* floor = app.CreateEntity();
-        NNE::Component::Physics::PlaneCollider const* PC = floor->AddComponent<NNE::Component::Physics::PlaneCollider>(glm::vec3(0, 1, 0), 10.0f);
+        //NNE::Component::Physics::PlaneCollider const* PC = floor->AddComponent<NNE::Component::Physics::PlaneCollider>(glm::vec3(0, 1, 0), 10.0f);
         NNE::Component::TransformComponent* TCfloor = floor->GetComponent<NNE::Component::TransformComponent>();
         NNE::Component::Physics::BoxColliderComponent const* BCFloor = floor->AddComponent<NNE::Component::Physics::BoxColliderComponent>(glm::vec3(100.0f, 0.50f, 100.0f));
-        NNE::Component::Physics::RigidbodyComponent const* RBCFloor = floor->AddComponent<NNE::Component::Physics::RigidbodyComponent>(0.0f, true);
+        NNE::Component::Physics::RigidbodyComponent const* RBCFloor = floor->AddComponent<NNE::Component::Physics::RigidbodyComponent>(10.0f, true);
 	
 	TCfloor->position = glm::vec3(0.0f, -3.0f, 0.0f);
 
@@ -17,24 +17,24 @@ int main() {
         NNE::Component::TransformComponent* TC = entity->GetComponent<NNE::Component::TransformComponent>();
         TC->position = glm::vec3(0.0f, 5.0f, 0.0f);
         NNE::Component::Physics::BoxColliderComponent const* BCC = entity->AddComponent<NNE::Component::Physics::BoxColliderComponent>(glm::vec3(1.0f, 1.0f, 1.0f));
-        NNE::Component::Physics::RigidbodyComponent const* RBC = entity->AddComponent<NNE::Component::Physics::RigidbodyComponent>( 0.1f, false);
+        NNE::Component::Physics::RigidbodyComponent const* RBC = entity->AddComponent<NNE::Component::Physics::RigidbodyComponent>( 0.1f, true);
 	
         MC->SetModelPath("../assets/models/viking_room.obj");
         MC->SetTexturePath("../assets/textures/viking_room.png");
-        /*
+        
         NNE::AEntity* player = app.CreateEntity();
         NNE::Component::TransformComponent* TCplayer = player->GetComponent<NNE::Component::TransformComponent>();
         NNE::Component::Physics::BoxColliderComponent* BCCplayer = player->AddComponent<NNE::Component::Physics::BoxColliderComponent>(glm::vec3(1.0f, 1.0f, 1.0f));
         NNE::Component::Physics::RigidbodyComponent* RBCplayer = player->AddComponent<NNE::Component::Physics::RigidbodyComponent>(1.0f, false);
-        PlayerController* PlayerC = player->AddComponent<PlayerController>();*/
+        PlayerController* PlayerC = player->AddComponent<PlayerController>();
 
-	//TCplayer->position = glm::vec3(0.0f, 0.0f, 5.0f);
+	TCplayer->position = glm::vec3(0.0f, 0.0f, 5.0f);
 
         NNE::AEntity* camera = app.CreateEntity();
         NNE::Component::Render::CameraComponent* CC = camera->AddComponent<NNE::Component::Render::CameraComponent>();
         NNE::Component::TransformComponent* TC2 = camera->GetComponent<NNE::Component::TransformComponent>();
 
-	//TC2->SetParent(TCplayer);
+	TC2->SetParent(TCplayer);
 
 	CC->SetPerspective(45.0f, 16.0f / 9.0f, 0.1f, 500.0f);	
 	TC2->position = glm::vec3(0.0f, 0.0f, 5.0f);
