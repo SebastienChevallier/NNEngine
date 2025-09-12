415ffc87f77b29904e5c526df47b2dd659b1fddc
Sebastien chevallier <44536776+SebastienChevallier@users.noreply.github.com>
2025-09-12 12:29:59 +0200

    Use Vulkan SDK environment variable for shader compilation

--- src/shaders/compile.bat
+++ src/shaders/compile.bat
@@ -1,7 +1,17 @@
-C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shader.vert -o vert.spv
-C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shader.frag -o frag.spv
+@echo off
+setlocal
 
-C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shadow.vert -o shadow_vert.spv
-C:/VulkanSDK/1.4.304.1/Bin/glslc.exe shadow.frag -o shadow_frag.spv
+REM Detect glslc path from VULKAN_SDK if available
+if defined VULKAN_SDK (
+    set "GLSLC=%VULKAN_SDK%\Bin\glslc.exe"
+) else (
+    echo [WARN] VULKAN_SDK not defined, using glslc from PATH.
+    set "GLSLC=glslc.exe"
+)
+
+"%GLSLC%" shader.vert -o vert.spv
+"%GLSLC%" shader.frag -o frag.spv
+"%GLSLC%" shadow.vert -o shadow_vert.spv
+"%GLSLC%" shadow.frag -o shadow_frag.spv
 
 pause
